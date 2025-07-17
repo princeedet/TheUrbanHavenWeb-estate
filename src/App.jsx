129f3af0c1e0c74a684820a224dff618c9274bf8
princeedet <princeedet190@gmail.com>
2025-07-17 12:23:01 -0400

    added the header, footer component and updated the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const App = () => {
   return (
-    <div className="text-red-700">
-        <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignOut() {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignOut
